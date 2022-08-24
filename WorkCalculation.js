8f87fdf94687ca8e3f457dbc7c5d486e25b6cff4
Artem0714 <arfed96@gmail.com>
2022-08-24 16:35:03 +0300

    BasePlanOfCalculationFinish

--- WorkCalculation.js
+++ WorkCalculation.js
@@ -1,7 +1,8 @@
 const buttons = document.querySelectorAll('button')
 const obj = {
     num1: [],
-    action: [],
+    sign: [],
+    signValue: [],
     num2: []
 };
 
@@ -10,32 +11,90 @@ const display = document.querySelector('input')
 buttons.forEach (elem => {
     elem.addEventListener('click', function getValue() {
         const value = elem.getAttribute('value')
-        const action = elem.hasAttribute('act')
+        const sign = elem.hasAttribute('act')
 
-        if (action) {
+        if (sign) {
             if (value === '='){
-                obj.num1 = parseInt(obj.num1) + parseInt(obj.num2)
+                obj.num2 = display.value
+                switch (obj.signValue) {
+                    case '+':
+                        obj.num1 = parseFloat(obj.num1) + parseFloat(obj.num2);
+                        break;
+                    case '-':
+                        obj.num1 = parseFloat(obj.num1) - parseFloat(obj.num2);
+                        break;
+                    case '*':
+                        obj.num1 = parseFloat(obj.num1) * parseFloat(obj.num2);
+                        break;
+                    case '/':
+                        if (parseFloat(obj.num2) === 0) {
+                            alert( 'Бесконечность' );
+                            obj.num1 = [];
+                            obj.num2 = [];
+                            obj.sign = [];
+                            obj.signValue = [];
+                        } else {
+                            obj.num1 = parseFloat(obj.num1) / parseFloat(obj.num2);//доработать деление на 0
+                        }
+                        break;
+                    default:
+                        console.log ('error');//возможно нужно убрать
+                }
                 obj.num1 = String(obj.num1)
                 display.value = obj.num1
-                console.log (obj.num1, obj.num2)
-                obj.action = []
+                obj.sign = []
                 obj.num2 = []
-            } else if (obj.action == true){
-                obj.action = action
-                obj.num1 = parseInt(obj.num1) + parseInt(obj.num2)
+            } else if (obj.sign == true){
+                obj.num2 = display.value
+                obj.sign = sign
+                obj.signValue = value
+                switch (obj.signValue) {
+                    case '+':
+                        obj.num1 = parseFloat(obj.num1) + parseFloat(obj.num2);
+                        break;
+                    case '-':
+                        obj.num1 = parseFloat(obj.num1) - parseFloat(obj.num2);
+                        break;
+                    case '*':
+                        obj.num1 = parseFloat(obj.num1) * parseFloat(obj.num2);
+                        break;
+                    case '/':
+                        if (parseFloat(obj.num2) === 0) {
+                            alert( 'Бесконечность' );
+                            obj.num1 = [];
+                            obj.num2 = [];
+                            obj.sign = [];
+                            obj.signValue = [];
+                        } else {
+                            obj.num1 = parseFloat(obj.num1) / parseFloat(obj.num2);//доработать деление на 0
+                        }
+                        break;
+                    default:
+                        console.log ('error');//возможно нужно убрать
+                }
                 obj.num1 = String(obj.num1)
                 display.value = []
                 obj.num2 = []
             } else {
-                obj.action = action
+                obj.num1 = display.value
+                obj.sign = sign
+                obj.signValue = value
                 display.value = []
             }
-        } else if (obj.action == true) {
+        } else if (obj.sign == true) {
             obj.num2 += value
-            display.value = obj.num2
+            if (value == '.') {
+                display.value = obj.num2 + '0' 
+            } else {
+                display.value = obj.num2
+            }
         } else {
             obj.num1 += value
-            display.value = obj.num1
+            if (value == '.') {
+                display.value = obj.num1 + '0' 
+            } else {
+                display.value = obj.num1
+            }            
         }
     })
 });
